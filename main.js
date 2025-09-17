b19282e4967e6340d08ffb71269e133731623cf1
Hassan <hachehri@gmail.com>
2025-09-17 16:38:52 +0100

    add function  to practice

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 
 
+//chapter 7
 let number1=10;
 let number2=5;
 let some=number1 + number2;
 
-console.log("The total is",some);
+console.log("The total is", some);
 
 
 
 
-let nom="Ali";
+let nom="Ali"; 
 const PI=3.14;
 console.log("Hello" + navigator);
 
@@ -24,3 +25,38 @@ console.log(a % b) // 1
 console.log(5 > 2);
 console.log(5 == "5");
 console.log(5 === "5");
+
+
+
+
+//Chapter 
+//function 
+
+function Name(){
+     console.log("Hello bro");
+};
+
+// call function Name()
+
+Name();
+
+
+//function with parameters
+
+function YourAga(age){
+      console.log("Your age is " , age);
+};
+
+YourAga(22);
+
+// return value
+
+function write(a,b){
+     return a + b;
+};
+
+let result=write(4,8);
+
+console.log(result); // 12
+
+
