bb0e47549a9ff048e1e9d874445d3a7bb725d134
Hassan <hachehri@gmail.com>
2025-09-17 13:12:50 +0100

    creat file html and creat js

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+let number1=10;
+let number2=5;
+let some=number1 + number2;
+
+console.log("The total is",some);
+
+
+
+
+let nom="Ali";
+const PI=3.14;
+console.log("Hello" + navigator);
+
+
+let a=10;
+let b=3;
+console.log(a + b ) // you can see in console 13
+console.log(a % b) // 1
+
+
+
+console.log(5 > 2);
+console.log(5 == "5");
+console.log(5 === "5");
+
